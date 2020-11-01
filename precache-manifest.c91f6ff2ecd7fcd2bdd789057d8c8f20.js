f631b4a7a7d6fa91be77928d6d60c1ffa6f22a77
YeoShiJing <yeo_shi_jing@hotmail.com>
2020-11-01 21:49:44 +0800

    Updates

---
+++ precache-manifest.c91f6ff2ecd7fcd2bdd789057d8c8f20.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b709eb25358f5c3107c52c6747b822e",
+    "url": "/cs4249/index.html"
+  },
+  {
+    "revision": "6dce0c3f00d48e53b5ea",
+    "url": "/cs4249/static/css/2.aa8e5226.chunk.css"
+  },
+  {
+    "revision": "569b27b5b9d3da3a86f4",
+    "url": "/cs4249/static/css/main.3d2b033f.chunk.css"
+  },
+  {
+    "revision": "6dce0c3f00d48e53b5ea",
+    "url": "/cs4249/static/js/2.59678d0d.chunk.js"
+  },
+  {
+    "revision": "4a16574f6c782381e56fe2d41e50025d",
+    "url": "/cs4249/static/js/2.59678d0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "569b27b5b9d3da3a86f4",
+    "url": "/cs4249/static/js/main.5e5f0658.chunk.js"
+  },
+  {
+    "revision": "acd82af67cd7c25a70fe",
+    "url": "/cs4249/static/js/runtime-main.4773b79f.js"
+  },
+  {
+    "revision": "db2f3ae103f718bb9a2b2f085ffefa09",
+    "url": "/cs4249/static/media/cart.db2f3ae1.svg"
+  },
+  {
+    "revision": "8f8f49549095fe5c6a8dd67deb4daf95",
+    "url": "/cs4249/static/media/check-delivry-rates.8f8f4954.png"
+  },
+  {
+    "revision": "d3ab11ddb2727ed678395dfe9ec6aaeb",
+    "url": "/cs4249/static/media/chinese_dabao_2.d3ab11dd.png"
+  },
+  {
+    "revision": "36b2bede84e7a308f440791111df6104",
+    "url": "/cs4249/static/media/chinese_delivery_2.36b2bede.png"
+  },
+  {
+    "revision": "a126aab474575cea42f3b32da52170c7",
+    "url": "/cs4249/static/media/custom.a126aab4.png"
+  },
+  {
+    "revision": "e693d390c0f6304d7d938d40afb451ae",
+    "url": "/cs4249/static/media/dabao_2.e693d390.png"
+  },
+  {
+    "revision": "ff7f517b3debf27629cea0421fe38cce",
+    "url": "/cs4249/static/media/delivery_2.ff7f517b.png"
+  },
+  {
+    "revision": "ac5a10d644eb518984a35bea746e9cc7",
+    "url": "/cs4249/static/media/delivery_address.ac5a10d6.png"
+  },
+  {
+    "revision": "bccb5d99827069269eed22c7c9c7026e",
+    "url": "/cs4249/static/media/driver.bccb5d99.png"
+  },
+  {
+    "revision": "781ee9ebb5100d8236f6eb1b2d39bbbc",
+    "url": "/cs4249/static/media/fb.781ee9eb.png"
+  },
+  {
+    "revision": "bca86764865abc48fcae88b884f1b129",
+    "url": "/cs4249/static/media/foodleh.bca86764.png"
+  },
+  {
+    "revision": "fb6d2f13c1e37255302944df4f53ee79",
+    "url": "/cs4249/static/media/gradient.fb6d2f13.png"
+  },
+  {
+    "revision": "caf700ce72075d916a87d20f5462470b",
+    "url": "/cs4249/static/media/hashtag.caf700ce.jpeg"
+  },
+  {
+    "revision": "c8e66f5f158c5b7d80ec07608e8f29aa",
+    "url": "/cs4249/static/media/home-bs.c8e66f5f.png"
+  },
+  {
+    "revision": "25975eeb30e228723f7316caaf1afe35",
+    "url": "/cs4249/static/media/i_want.25975eeb.jpeg"
+  },
+  {
+    "revision": "fe4b3f4ddf71cd68c2aa40510fc48383",
+    "url": "/cs4249/static/media/info_delivery.fe4b3f4d.png"
+  },
+  {
+    "revision": "0cfc412a44773a3ad751de7469106fb5",
+    "url": "/cs4249/static/media/info_menu.0cfc412a.png"
+  },
+  {
+    "revision": "611fed971217b986cbd3e2567afd5bbd",
+    "url": "/cs4249/static/media/info_review_order.611fed97.png"
+  },
+  {
+    "revision": "dbbfaf1d38de96d18d4c9c9c72221474",
+    "url": "/cs4249/static/media/infographic.dbbfaf1d.jpg"
+  },
+  {
+    "revision": "b2aa37d3e49ec257c51dd7875903644b",
+    "url": "/cs4249/static/media/insta.b2aa37d3.png"
+  },
+  {
+    "revision": "1924a4dc75b28d24df8d0eb9cbc90f84",
+    "url": "/cs4249/static/media/list_cn.1924a4dc.png"
+  },
+  {
+    "revision": "225dbeeb2ee62590b5abec46292c6585",
+    "url": "/cs4249/static/media/list_en.225dbeeb.png"
+  },
+  {
+    "revision": "2982e06916c8bf25ca698399b445b23c",
+    "url": "/cs4249/static/media/logo-black.2982e069.png"
+  },
+  {
+    "revision": "621c3aabaf51a49f2d0309205ef414cc",
+    "url": "/cs4249/static/media/logo-header-nologo.621c3aab.png"
+  },
+  {
+    "revision": "8c84f96a696ad13e3b300d885b048dad",
+    "url": "/cs4249/static/media/motor-delivery.8c84f96a.png"
+  },
+  {
+    "revision": "85bd227c35c63c92c11a4fabf76d1f2c",
+    "url": "/cs4249/static/media/mrt_logo.85bd227c.png"
+  },
+  {
+    "revision": "348985c2c2c7036e2d8539d317cbacbf",
+    "url": "/cs4249/static/media/orderleh_title.348985c2.png"
+  },
+  {
+    "revision": "1f25c33ece639467ba576f730bfd0c0d",
+    "url": "/cs4249/static/media/placeholder.1f25c33e.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/cs4249/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/cs4249/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/cs4249/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "d5892e0395cfd0fbcefb7cbb8cb2cc87",
+    "url": "/cs4249/static/media/store_address.d5892e03.png"
+  },
+  {
+    "revision": "e3b1abbda308b61952da87f3b5bc2999",
+    "url": "/cs4249/static/media/website_cn.e3b1abbd.png"
+  },
+  {
+    "revision": "f8b1d32e9430e960c8e325d8c061128c",
+    "url": "/cs4249/static/media/website_en.f8b1d32e.png"
+  },
+  {
+    "revision": "9500e0262fb431afc7ba68c9390e2c19",
+    "url": "/cs4249/static/media/whatsapp_icon.9500e026.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.eb014ccf5a938615631f20dbafcf861e.js"
+  "/cs4249/precache-manifest.c91f6ff2ecd7fcd2bdd789057d8c8f20.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cs4249/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
